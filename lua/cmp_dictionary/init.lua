e5c56170fdc1e757c9679217f7d469bd54740740
uga-rosa <uga6603@gmail.com>
2021-11-29 16:46:25 +0900

    Add async option to support very large dictionary

--- lua/cmp_dictionary/init.lua
+++ lua/cmp_dictionary/init.lua
@@ -5,20 +5,15 @@ end
 if vim.g.cmp_dictionary_exact == nil then
     vim.g.cmp_dictionary_exact = 2
 end
-
--- util
-local function echo(msg)
-    if not vim.g.cmp_dictionary_silent then
-        print("[cmp-dictionary] " .. msg)
-    end
+if vim.g.cmp_dictionary_asnyc == nil then
+    vim.g.cmp_dictionary_asnyc = false
 end
 
-local function tbl_len(tbl)
-    local res = 0
-    for _ in pairs(tbl) do
-        res = res + 1
+-- util
+local function echo(msg, force)
+    if force or not vim.g.cmp_dictionary_silent then
+        print("[cmp-dictionary] " .. msg)
     end
-    return res
 end
 
 -- body
@@ -27,6 +22,7 @@ local source = {}
 local f = vim.fn
 local a = vim.api
 local uv = vim.loop
+local ok, mpack = pcall(require, "mpack")
 
 function source.new()
     return setmetatable({}, { __index = source })
@@ -35,13 +31,169 @@ end
 local post_dic, dictionaries
 local items = {}
 local indexes = {}
-local loaded = false
 
 function source:is_available()
-    return loaded
+    return #items ~= 0
 end
 
-source.read_dictionary = function()
+local index_async = uv.new_work(function(buffer, exact)
+    ---@diagnostic disable-next-line: redefined-local
+    local mpack = require("mpack")
+
+    -- Since it is a separate thread, global variables such as `vim` cannot be used.
+    local function gsplit(s, sep, plain)
+        local start = 1
+        local done = false
+
+        local function _pass(i, j, ...)
+            if i then
+                assert(j + 1 > start, "Infinite loop detected")
+                local seg = s:sub(start, i - 1)
+                start = j + 1
+                return seg, ...
+            else
+                done = true
+                return s:sub(start)
+            end
+        end
+
+        return function()
+            if done or (s == "" and sep == "") then
+                return
+            end
+            if sep == "" then
+                if start == #s then
+                    done = true
+                end
+                return _pass(start + 1, start)
+            end
+            return _pass(s:find(sep, start, plain))
+        end
+    end
+
+    local items_ = {}
+
+    for name, data in pairs(mpack.Unpacker()(buffer)) do
+        local detail = "belong to `" .. name .. "`"
+        for w in gsplit(data, "%s+") do
+            if w ~= "" then
+                table.insert(items_, { label = w, detail = detail })
+            end
+        end
+    end
+
+    if #items_ == 0 then
+        return
+    end
+
+    table.sort(items_, function(item1, item2)
+        return item1.label < item2.label
+    end)
+
+    if exact == -1 then
+        for _, i in pairs(items_) do
+            if exact < #i.label then
+                exact = #i.label
+            end
+        end
+    end
+
+    local indexes_ = {}
+
+    for len = 1, exact do
+        local s = 1
+        while #items_[s].label < len do
+            s = s + 1
+        end
+        local _pre = items_[s].label:sub(1, len)
+        indexes_[_pre] = { start = s }
+        local pre
+        for j = s + 1, #items_ do
+            local i = items_[j].label
+            if #i >= len then
+                pre = i:sub(1, len)
+                if pre ~= _pre then
+                    if indexes_[_pre].last == nil then
+                        indexes_[_pre].last = j - 1
+                    end
+                    indexes_[pre] = { start = j }
+                    _pre = pre
+                end
+            elseif indexes_[_pre].last == nil then
+                indexes_[_pre].last = j - 1
+            end
+        end
+        indexes_[_pre].last = #items_
+    end
+
+    return mpack.Packer()(items_), mpack.Packer()(indexes_)
+end, function(items_, indexes_)
+    if items_ then
+        items = mpack.Unpacker()(items_)
+        indexes = mpack.Unpacker()(indexes_)
+        echo("All dictionaries are loaded")
+    else
+        echo("Only empty dictionaries")
+    end
+end)
+
+local index_sync = function(buffer, exact)
+    for name, data in pairs(buffer) do
+        local detail = "belong to `" .. name .. "`"
+        for w in vim.gsplit(data, "%s+") do
+            if w ~= "" then
+                table.insert(items, { label = w, detail = detail })
+            end
+        end
+    end
+
+    if #items == 0 then
+        echo("Only empty dictionaries")
+        return
+    end
+
+    table.sort(items, function(item1, item2)
+        return item1.label < item2.label
+    end)
+
+    if exact == -1 then
+        for _, i in pairs(items) do
+            if exact < #i.label then
+                exact = #i.label
+            end
+        end
+    end
+
+    for len = 1, exact do
+        local s = 1
+        while #items[s].label < len do
+            s = s + 1
+        end
+        local _pre = items[s].label:sub(1, len)
+        indexes[_pre] = { start = s }
+        local pre
+        for j = s + 1, #items do
+            local i = items[j].label
+            if #i >= len then
+                pre = i:sub(1, len)
+                if pre ~= _pre then
+                    if indexes[_pre].last == nil then
+                        indexes[_pre].last = j - 1
+                    end
+                    indexes[pre] = { start = j }
+                    _pre = pre
+                end
+            elseif indexes[_pre].last == nil then
+                indexes[_pre].last = j - 1
+            end
+        end
+        indexes[_pre].last = #items
+    end
+
+    echo("All dictionaries are loaded")
+end
+
+function source.update()
     post_dic = dictionaries
 
     do
@@ -54,111 +206,67 @@ source.read_dictionary = function()
         return
     end
 
-    local paths = (function()
-        if dictionaries == "" then
-            return {}
-        end
-        local result = {}
-        local dics = vim.split(dictionaries, ",")
-        for _, dic in ipairs(dics) do
+    items = {}
+    indexes = {}
+
+    local paths = {}
+    local number_of_paths = 0
+
+    if dictionaries ~= "" then
+        for dic in vim.gsplit(dictionaries, ",") do
             local path = f.expand(dic)
             if f.filereadable(path) == 1 then
                 local name = f.fnamemodify(path, ":t")
-                result[name] = path
+                paths[name] = path
+                number_of_paths = number_of_paths + 1
             else
                 echo("No such file: " .. path)
             end
         end
-        return result
-    end)()
+    end
 
-    if tbl_len(paths) == 0 then
+    if number_of_paths == 0 then
         echo("No dictionary loaded")
-        loaded = false
         return
     end
 
-    local datas = {}
+    local read_count = 0
+    local buffers = {}
 
     for name, path in pairs(paths) do
         uv.fs_open(path, "r", 438, function(err, fd)
             assert(not err, err)
             uv.fs_fstat(fd, function(err2, stat)
                 assert(not err2, err2)
-                uv.fs_read(fd, stat.size, 0, function(err3, data)
+                uv.fs_read(fd, stat.size, 0, function(err3, buffer)
                     assert(not err3, err3)
                     uv.fs_close(fd, function(err4)
                         assert(not err4, err4)
-                        datas[name] = data
+                        buffers[name] = buffer
+                        echo(path .. " are loaded")
+                        read_count = read_count + 1
                     end)
                 end)
             end)
         end)
     end
 
-    items = {}
-
     local timer = uv.new_timer()
     timer:start(0, 100, function()
-        if tbl_len(datas) == tbl_len(paths) then
-            for name, data in pairs(datas) do
-                local detail = "belong to `" .. name .. "`"
-                for w in vim.gsplit(data, "%s+") do
-                    if w ~= "" then
-                        table.insert(items, { label = w, detail = detail })
-                    end
-                end
-            end
-
-            if #items == 0 then
-                timer:close()
-                loaded = false
-                echo("Only empty dictionaries")
-                return
-            end
-
-            table.sort(items, function(item1, item2)
-                return item1.label < item2.label
-            end)
-
-            local max_len = vim.g.cmp_dictionary_exact
-            if max_len == -1 then
-                for _, item in pairs(items) do
-                    if max_len < #item.label then
-                        max_len = #item.label
-                    end
-                end
-            end
-
-            for len = 1, max_len do
-                local s = 1
-                while #items[s].label < len do
-                    s = s + 1
-                end
-                local _pre = items[s].label:sub(1, len)
-                indexes[_pre] = { start = s }
-                local pre
-                for j = s + 1, #items do
-                    local item = items[j].label
-                    if #item >= len then
-                        pre = item:sub(1, len)
-                        if pre ~= _pre then
-                            if indexes[_pre].last == nil then
-                                indexes[_pre].last = j - 1
-                            end
-                            indexes[pre] = { start = j }
-                            _pre = pre
-                        end
-                    elseif indexes[_pre].last == nil then
-                        indexes[_pre].last = j - 1
-                    end
-                end
-                indexes[_pre].last = #items
-            end
-
+        if read_count == number_of_paths then
+            timer:stop()
             timer:close()
-            loaded = true
-            echo("All dictionaries are loaded")
+            if vim.g.cmp_dictionary_asnyc then
+                if ok then
+                    echo("Run asynchronously")
+                    index_async:queue(mpack.Packer()(buffers), vim.g.cmp_dictionary_exact)
+                    return
+                else
+                    echo("Module `mpack` is not available", true)
+                end
+            end
+            echo("Run synchronously")
+            index_sync(buffers, vim.g.cmp_dictionary_exact)
         end
     end)
 end
