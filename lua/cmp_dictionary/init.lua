0db0eec87aca8a3c1a27e1182b1843fb0a619497
uga-rosa <uga6603@gmail.com>
2022-11-01 06:51:33 +0900

    refactor: separate document functions

--- lua/cmp_dictionary/init.lua
+++ lua/cmp_dictionary/init.lua
@@ -1,7 +1,5 @@
 local source = {}
 
-local cmp = require("cmp")
-
 local utf8 = require("cmp_dictionary.lib.utf8")
 local caches = require("cmp_dictionary.caches")
 local config = require("cmp_dictionary.config")
@@ -143,73 +141,8 @@ function source:complete(request, callback)
     callback(source.get_candidate(req, isIncomplete))
 end
 
-local document_cache = require("cmp_dictionary.lfu").init(100)
-
-local function get_command(word)
-    local command = config.get("document_command")
-
-    local args
-    if type(command) == "table" then
-        -- copy
-        args = {}
-        for i, v in ipairs(command) do
-            args[i] = v
-        end
-    elseif type(command) == "string" then
-        args = vim.split(command, " ")
-    end
-
-    local cmd = table.remove(args, 1)
-    for i, arg in ipairs(args) do
-        if arg:find("%s", 1, true) then
-            args[i] = arg:format(word)
-        end
-    end
-
-    return cmd, args
-end
-
-local function get_document(completion_item, callback)
-    local ok, Job = pcall(require, "plenary.job")
-    if not ok then
-        vim.notify("[cmp-dictionary]: document feature requires plenary.nvim")
-        return
-    end
-
-    local word = completion_item.label
-    local command, args = get_command(word)
-    if not command then
-        callback(completion_item)
-        return
-    end
-
-    Job:new({
-        command = command,
-        args = args,
-        on_exit = function(j)
-            local result = table.concat(j:result(), "\n")
-            document_cache:set(word, result)
-            completion_item.documentation = result
-            callback(completion_item)
-        end
-    }):start()
-end
-
 function source:resolve(completion_item, callback)
-    if config.get("document") then
-        local cached = document_cache:get(completion_item.label)
-        if cached then
-            completion_item.documentation = {
-                kind = cmp.lsp.MarkupKind.PlainText,
-                value = cached,
-            }
-            callback(completion_item)
-        else
-            get_document(completion_item, callback)
-        end
-    else
-        callback(completion_item)
-    end
+    require("cmp_dictionary.document")(completion_item, callback)
 end
 
 function source.setup(opt)
