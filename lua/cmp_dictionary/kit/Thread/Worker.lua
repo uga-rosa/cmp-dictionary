83e0391a6a5cf270b2284444045a30beb0769b97
uga-rosa <uga6603@gmail.com>
2023-04-01 03:23:05 +0900

    chore: update nvim-kit

--- lua/cmp_dictionary/kit/Thread/Worker.lua
+++ lua/cmp_dictionary/kit/Thread/Worker.lua
@@ -18,7 +18,7 @@ end
 ---Call worker function.
 ---@return cmp_dictionary.kit.Async.AsyncTask
 function Worker:__call(...)
-  local args = { ... }
+  local args_ = { ... }
   return AsyncTask.new(function(resolve, reject)
     uv.new_work(function(runner, args, option)
       args = vim.mpack.decode(args)
@@ -51,7 +51,7 @@ function Worker:__call(...)
       end
     end):queue(
       self.runner,
-      vim.mpack.encode(args),
+      vim.mpack.encode(args_),
       vim.mpack.encode({
         cwd = uv.cwd(),
       })
