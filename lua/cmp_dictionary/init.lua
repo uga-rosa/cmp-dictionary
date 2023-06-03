1e0ebd9cd32d691f1d9717a17c3246de9ebcba79
uga-rosa <uga6603@gmail.com>
2023-06-03 21:47:03 +0900

    refactor: reduce autocmd

--- lua/cmp_dictionary/init.lua
+++ lua/cmp_dictionary/init.lua
@@ -37,42 +37,38 @@ function M.switcher(opt)
 
   local id = vim.api.nvim_create_augroup("cmp_dictionary", {})
 
-  vim.api.nvim_create_autocmd("User", {
-    group = id,
-    pattern = "cmp_dictionary",
-    callback = function()
-      if opt.filetype then
-        vim.opt_local.dictionary:append(opt.filetype[vim.bo.filetype] or "")
-      end
-      if opt.filepath then
-        local fullpath = vim.fn.expand("%:p")
-        for path, dict in pairs(opt.filepath) do
-          if fullpath:find(path) then
-            vim.opt_local.dictionary:append(dict)
-          end
+  local function callback()
+    if opt.filetype then
+      vim.opt_local.dictionary:append(opt.filetype[vim.bo.filetype] or "")
+    end
+    if opt.filepath then
+      local fullpath = vim.fn.expand("%:p")
+      for path, dict in pairs(opt.filepath) do
+        if fullpath:find(path) then
+          vim.opt_local.dictionary:append(dict)
         end
       end
-      if opt.spelllang then
-        for _, sl in ipairs(vim.opt.spelllang:get()) do
-          vim.opt_local.dictionary:append(opt.spelllang[sl] or "")
-        end
+    end
+    if opt.spelllang then
+      for _, sl in ipairs(vim.opt.spelllang:get()) do
+        vim.opt_local.dictionary:append(opt.spelllang[sl] or "")
       end
-      M.update()
-    end,
-  })
+    end
+    M.update()
+  end
 
   if opt.filetype then
     vim.api.nvim_create_autocmd("FileType", {
       group = id,
       pattern = vim.tbl_keys(opt.filetype),
-      command = "do User cmp_dictionary",
+      callback = callback,
     })
   end
 
   if opt.filepath then
     vim.api.nvim_create_autocmd("BufEnter", {
       group = id,
-      command = "do User cmp_dictionary",
+      callback = callback,
     })
   end
 
@@ -80,11 +76,11 @@ function M.switcher(opt)
     vim.api.nvim_create_autocmd("OptionSet", {
       group = id,
       pattern = "spelllang",
-      command = "do User cmp_dictionary",
+      callback = callback,
     })
   end
 
-  vim.cmd("do User cmp_dictionary")
+  callback()
 end
 
 return M
