54be2f4591e3c0dd06ad6fe61d44f9e82ad3e266
uga-rosa <uga6603@gmail.com>
2024-01-21 21:20:49 +0900

    notify for switcher

--- lua/cmp_dictionary/init.lua
+++ lua/cmp_dictionary/init.lua
@@ -1,5 +1,8 @@
 return {
   setup = require("cmp_dictionary.config").setup,
+  switcher = function()
+    vim.notify("[cmp-dictionary] switcher is deprecated.\nSee `:h cmp-dictionary-option-paths`", vim.log.levels.ERROR)
+  end,
   -- Override in after/plugin/cmp_dictionary.lua
   update = function() end,
 }
